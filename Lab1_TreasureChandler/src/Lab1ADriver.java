c3da77c5c00f8b7d94cd5796339197c63003c990
Treasure-Chandler <tchandler1211@gmail.com>
2025-01-14 11:30:30 -0500

    Finished Lab1A!

--- Lab1_TreasureChandler/src/Lab1ADriver.java
+++ Lab1_TreasureChandler/src/Lab1ADriver.java
@@ -2,8 +2,8 @@
  * @author Treasure Chandler
  */
 
-import java.io.*;               // Mainly used for file reading stuff
-import java.util.Scanner;       // Needed for the Scanner class
+import java.io.*;       // Mainly used for file reading stuff
+import java.util.*;     // Used for the Scanner, ArrayList, and List classes
 
 public class Lab1ADriver {
     /**
@@ -12,7 +12,42 @@ public class Lab1ADriver {
      */
     public static void main(String[] args) {
         // Variables declaration
-        Rectangle[] rectangles;
-        
+        Rectangle[] rectangles = null;
+        String rectangleFile = "rectangles.txt";
+
+        try (Scanner rectangleScan = new Scanner(new File(rectangleFile))) {
+            // Read the total number of lines in the file
+            int rectangleAmount = rectangleScan.nextInt();
+            rectangleScan.nextLine();
+
+            // Initialize the object array with the given size
+            rectangles = new Rectangle[rectangleAmount];
+
+            // Parse the rectangle data and store it in the array
+            for (int i = 0; i < rectangleAmount; i++) {
+                if (rectangleScan.hasNextLine()) {
+                    // Read the lines
+                    String line = rectangleScan.nextLine();
+                    String[] bits = line.split("\\s+");
+
+                    // Read the status of the rows, columns, and filled/unfilled
+                    int numRows = Integer.parseInt(bits[0]);
+                    int numCols = Integer.parseInt(bits[1]);
+                    boolean filled = bits[2].equalsIgnoreCase("filled");
+
+                    // Instantiate a new rectangle object to store in the array
+                    rectangles[i] = new Rectangle(numRows, numCols, filled);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File \"rectangles.txt\" not found: " + e.getMessage());
+        }
+
+        // Print the rectangles
+        if (rectangles != null) {
+            for (Rectangle rectangle : rectangles) {
+                System.out.println(rectangle);
+            }
+        }
     } // End of main()
 } // End of Lab1ADriver
--- Lab1_TreasureChandler/src/Rectangle.java
+++ Lab1_TreasureChandler/src/Rectangle.java
@@ -4,9 +4,9 @@
 
 public class Rectangle {
     // Variables declaration
-    int numRows; // Store the number of rows
-    int numCols; // Store the number of columns
-    boolean filled; // Defines either a filled or unfilled rectangle
+    private int numRows; // Store the number of rows
+    private int numCols; // Store the number of columns
+    private boolean filled; // Defines either a filled or unfilled rectangle
 
     // Initializes variables to their default values
     public Rectangle() {
@@ -16,8 +16,10 @@ public class Rectangle {
     } // End of Rectangle constructor
 
     // Values input will be the parameters for this constructor
-    public Rectangle(int recRow, int recCol) {
-
+    public Rectangle(int recRow, int recCol, boolean fill) {
+        this.numRows = recRow;
+        this.numCols = recCol;
+        this.filled = fill;
     } // End of Rectangle constructor
 
     /**
@@ -56,18 +58,25 @@ public class Rectangle {
      * Create and return a string for console output
      * @return      The formatted string
      */
+    @Override
     public String toString() {
         // Variables declaration
         String rectangleChar = "";
 
+        /* 
+         * Configures the rows and columns into "coordinates" using nested
+         * for loops
+         */
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numCols; j++) {
-                if (!filled) {
-                    if (i == 1 || i == numRows) {
-
+                    // Filled logic
+                    if (filled || i == 0 || i == numRows - 1 || j == 0 || j == numCols - 1) {
+                        rectangleChar += "#";
+                    } else {
+                        rectangleChar += " ";
                     }
-                }
             }
+            rectangleChar += "\n";
         }
 
         return rectangleChar;
